3de2c8bdcb984291e009a9227ff9183408707564
awkward-3312 <betza_es77@hotmail.com>
2025-11-19 18:38:59 -0600

    updates
    agregue heros, imagenes de home para vision rapida del cliente

--- home/script.js
+++ home/script.js
@@ -29,3 +29,14 @@
     });
   })();
   
+  // SLIDER AUTOMÁTICO HERO
+let slideIndex = 0;
+const slides = document.querySelectorAll('.hero-slider .slide');
+
+function changeSlide() {
+    slides[slideIndex].classList.remove('active');
+    slideIndex = (slideIndex + 1) % slides.length;
+    slides[slideIndex].classList.add('active');
+}
+
+setInterval(changeSlide, 7000); // Cambia los segundos
